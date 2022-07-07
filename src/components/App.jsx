72d129fe8f145bbc3446b7e2fd8c5b53fe77a219
AntonTarasiuk <tarasiuk.anton@gmail.com>
2022-07-07 18:14:00 +0300

    Added statistics of feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import { Feedbacks } from "./FeedbackStats/FeedbackStats";
+
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        textAlign: 'left',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
+        fontSize: 20,
         color: '#010101'
       }}
     >
-      React homework template
+      <Feedbacks />
     </div>
   );
 };
